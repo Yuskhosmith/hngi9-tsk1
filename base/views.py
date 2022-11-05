840d6f78729e7c88600fa3fe246d7b87fba4d8af
Yukhosmith <yuskhosmith1@gmail.com>
2022-11-05 14:56:14 +0100

    Task 2

--- base/views.py
+++ base/views.py
@@ -1,6 +1,5 @@
-from tkinter import Y
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
